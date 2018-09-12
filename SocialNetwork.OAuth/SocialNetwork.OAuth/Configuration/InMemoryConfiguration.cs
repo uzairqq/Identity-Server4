ae9f4aae6ab3bb83e7ea4a88456cfb1e0f245a60
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-12 16:56:11 +0500

    Added useIdentityServer property in ConfigureServices to use identty server and added new folder configuration with a new class called InMemoryonfiguration for api resources

---
+++ SocialNetwork.OAuth/SocialNetwork.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace SocialNetwork.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("socialnetwork", "Social Network")
+            };
+        }
+    }
+}
